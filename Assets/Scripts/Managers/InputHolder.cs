ecb5d44209704f98107d9c11be6381ce6d0e7843
Nader <nader.ma@live.com>
2018-06-02 00:41:38 +0200

    minor modifications

--- Assets/Scripts/Managers/InputHolder.cs
+++ Assets/Scripts/Managers/InputHolder.cs
@@ -15,6 +15,28 @@ public class InputHolder : MonoBehaviour {
         fm = FindObjectOfType<FloorManager>();
     }
 
+    private void Update()
+    {
+        if(Input.touchCount == 1)
+        {
+            var finger = Input.touches;
+            if (finger[0].phase == TouchPhase.Stationary || finger[0].phase == TouchPhase.Began)
+            {
+                var ray = Camera.main.ScreenPointToRay(new Vector3(finger[0].position.x, finger[0].position.y));
+                RaycastHit info;
+                if (Physics.Raycast(ray, out info, 300))
+                {
+                    if (info.collider.tag == "Worker")
+                    {
+                        info.collider.gameObject.GetComponent<ClickableWorker>().ClickWorker();
+                    }
+                }
+            }
+
+        }
+    }
+
+
     public void SetFloorTouchActive(bool floorTouchOn)
     {
         //fm.SetFloorTouchActive(floorTouchOn);
