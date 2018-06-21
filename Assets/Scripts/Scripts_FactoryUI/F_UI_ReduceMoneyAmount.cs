1868e168535bcd1c9b8a223a261daacf9b306c8b
Safwa <safwa.tawab@gmail.com>
2018-06-21 22:48:36 +0200

    Fixing machine fixing cost and its animation

---
+++ Assets/Scripts/Scripts_FactoryUI/F_UI_ReduceMoneyAmount.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_UI_ReduceMoneyAmount : MonoBehaviour {
+
+	public void DisableMoneyCost()
+    {
+        gameObject.SetActive(false);
+    }
+}
